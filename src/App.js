c78bebaf742da58c258ce7118cd4fcdba81b0fbd
DiegoBastos40 <diegobastosherval@gmail.com>
2022-03-03 19:26:29 +0000

    done without style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import countries from './countries.json'
+import Navbar from './Components/Navbar';
+import CountriesList from './Components/CountriesList';
+import axios from 'axios';
+import { Routes,Route } from 'react-router-dom';
+import CountryDetails from './Components/CountryDetails';
+
 
 function App() {
+  const [myCountries, setMyCountries] = useState([]);
+  console.log(myCountries)
+  
+useEffect(() => {
+    axios
+      .get(' https://ih-countries-api.herokuapp.com/countries')
+      .then((response) => {
+        setMyCountries(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="App">
+    <div className="App-main">
+   
+      <Navbar countries={myCountries}/>
+    
+      </div>
+      <Routes>
+      <Route path='/' element={<CountriesList countries={myCountries} />}/>
+      <Route path=":alpha3Code" element={<CountryDetails countries={myCountries} />}/>
+      </Routes>
+ 
+   
+      
     </div>
   );
+ 
 }
 
 export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navbar(props) {
+  return (
+    <nav className="Nav-header">
+    <h1> LAB-WikiCountries !</h1>
+  </nav>
+  
+  )
+}
